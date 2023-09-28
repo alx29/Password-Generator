74b59ef50ac52ea3f4a854cf31fdbc40a2b13ea3
Alex Paraschiv <aparaschiv@adobe.com>
2023-09-29 00:13:33 +0300

    made copy to clipboard to work

--- src/components/PasswordHeader.tsx
+++ src/components/PasswordHeader.tsx
@@ -1,14 +1,46 @@
 import './PasswordHeader.css';
 import Copy from "../assets/icon-copy.svg?react";
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+
+const copyToCLipboard = (textToCopy: string) => {
+  const textArea = document.createElement('textarea');
+  textArea.value = textToCopy;
+  document.body.appendChild(textArea);
+
+  // Select the text in the input element
+  textArea.select();
+
+  try {
+    // Execute the copy command
+    document.execCommand('copy');
+  } catch (err) {
+    console.error(err);
+  }
+
+  // Remove the temporary input element
+  document.body.removeChild(textArea);
+}
 
 const PasswordHeader = () => {
+  const [copied, setCopied] = useState(false);
   const password = useSelector((state) => state.passwordGenerator.password);
+
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 500);
+    copyToCLipboard(password);
+  }
 
   return (
     <div className="PasswordHeader">
         <div className='password'>{password}</div>
-        <Copy className="copyIcon"/>
+        <div className='copyContainer'>
+          {copied && <div className='copyText'>COPIED</div>}
+          <Copy className="copyIcon" onClick={handleCopy}/>
+        </div>
     </div>
   )
 }
